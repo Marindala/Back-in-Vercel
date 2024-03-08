6d105e250c27ef6abda34da651b5b42a2958a793
Marina <95050756+Marindala@users.noreply.github.com>
2024-03-08 02:00:27 -0300

    first commit

---
+++ index.js
@@ -0,0 +1,25 @@
+const express = require ('express');
+
+const app = express();
+const port =process.env.PORT || 3000;
+
+app.get("/", (req, res) => {
+    const htmlResponse = `
+    <html>
+        <head>
+            <title> Back en Vercel</title>
+        </head>
+        <body>
+            <h1>Proyect Back in Vercel</h1>
+        </body>
+    </html>
+    `;
+    res.send(htmlResponse);
+//
+//
+            
+});
+
+app.listen(port, () => {
+  console.log(`port runing in http://localhost:${port}`);
+});
